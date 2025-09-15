5a4b81911bcda6d891abb8f26c570468f497b7ac
Bhagesh07 <bhageshchavan4304@gmail.com>
2025-09-15 22:33:24 +0530

    Clean up next.config.js - remove deprecated appDir flag

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true, // Tell Next.js to use the App Router in src/app
-  },
+  // You can add other configs here if needed
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
